b34e97131076d19750d2dfa727b588bd319d3079
Aditya <adityabohra619@gmail.com>
2023-12-06 12:52:36 +0530

    Added TaskItem

--- src/Task.js
+++ src/Task.js
@@ -6,6 +6,45 @@ import { MdSettings } from 'react-icons/md'
 import { FiMoreVertical } from "react-icons/fi";
 import { useDrag } from 'react-dnd'
 import { useState, useEffect } from 'react';
+import { Input } from '@chakra-ui/react';
+const TaskItem = (props) => {
+    const [settingsOpen, setSettingsOpen] = useState(false);
+    if (!settingsOpen)
+        return <Box padding={5} borderRadius={10} marginBottom={5} background={'white'} width={'380px'}>
+            <Flex justifyContent={'center'} alignItems={'center'} width={'380xp'}>
+                <CheckCircleIcon color={'silver'} fontSize={28} ></CheckCircleIcon>
+                <Text fontSize={18} fontWeight={'bold'} flexBasis={'70%'} ml={2}>{props.title}</Text>
+                <Text color={'gray'} fontWeight={'semi-bold'}>0/2</Text>
+                <IconButton ml={2} p={0}><Icon as={FiMoreVertical} fontSize={25} onClick={() => { setSettingsOpen(true) }}></Icon></IconButton>
+            </Flex>
+            <Text backgroundColor={'wheat'} padding={2} mt={2} borderRadius={3}>{props.description}</Text>
+        </Box>
+    else
+        return <Box padding={5} borderRadius={10} marginBottom={5} background={'white'} width={'380px'}>
+            <Box> <input></input>
+                <p>Act/Est Pomodoros</p>
+                <Flex>
+                    <Input placeholder='Name'></Input>
+                    <Input></Input>
+                    <button></button>
+                    <button></button>
+                </Flex>
+                <Flex>
+                    <button>Add note</button>
+                    <button>Add Project</button>
+                </Flex>
+            </Box>
+            <Box>
+                <button>Delete</button>
+                <Flex>
+                    <button>cancel</button>
+                    <button>save</button>
+                </Flex>
+
+            </Box>
+        </Box>
+}
+
 const Task = () => {
     const { isLoading, isSuccess, error, data, isError, isFetching } = useFetchTaskQuery()
     const taskSelected = useTaskSelectedQuery()
@@ -26,17 +65,11 @@ const Task = () => {
 
         }} width={'380xp'} >
             <VStack >
+            <TaskItem description={''} title={''}></TaskItem>
+
                 {isSuccess && data.map(item =>
                     <Radio value={item.id} key={item.id}  >
-                        <Box padding={5} borderRadius={10} marginBottom={5} background={'white'} width={'380px'}>
-                            <Flex justifyContent={'center'} alignItems={'center'} width={'380xp'}>
-                                <CheckCircleIcon color={'silver'} fontSize={28} ></CheckCircleIcon>
-                                <Text fontSize={18} fontWeight={'bold'} flexBasis={'70%'} ml={2}>{item.title}</Text>
-                                <Text color={'gray'} fontWeight={'semi-bold'}>0/2</Text>
-                                <IconButton ml={2} p={0}><Icon as={FiMoreVertical} fontSize={25}></Icon></IconButton>
-                            </Flex>
-                            <Text backgroundColor={'wheat'} padding={2} mt={2} borderRadius={3}>{item.description}</Text>
-                        </Box>
+                        <TaskItem description={item.description} title={item.title}></TaskItem>
                     </Radio>
 
 
